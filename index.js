ef3137eee13afc52df849e5027a430cf3cbd4a27
Thiagouh <thiagolorhandasilva12@gmail.com>
2025-03-12 15:04:49 -0300

    first commit

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/public/index.html')
 });
-
-
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
